83bb0ed4c4cf2e17dc708e4a1176d240d350a0ff
LIUJE7 <LIUJE7@oocl.cpm>
2019-07-10 13:29:16 +0800

    修改

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,7 +1,7 @@
 const isStartNumberSmallThanEndNumber = require('../main');
 const isInRangeOfOneToThousand= require('../main');
 const createMuiltiplicationResult= require('../main');
-
+const printMuiltplicationTable= require('../main');
 it ('should return true when startnumber smaller than endnumber', () => {
     expect( isStartNumberSmallThanEndNumber(2, 3)).toBe(true);
 });
@@ -11,6 +11,15 @@ it ('should return true when startnumber is equal to endnumber', () => {
 it ('should return true when startnumber and endnumber can be any number in a range of 1 to 1000 ', () => {
     expect(isInRangeOfOneToThousand(4, 5)).toBe(true);
 });
-it ('should return string when startnumber is 2 and endnumber is 4,the output should be something like 2*2...', () => {
-          expect(createMuiltiplicationResult(2, 4)).toBe({"2*2","2*3","2*4","3*3","3*4","4*4"});
+it ('should return equal when startnumber is 2 and endnumber is 4,the output should be something like 2*2...', () => {
+          expect(createMuiltiplicationResult(2, 4)).toBe("2*2=4\r\n2*3=6  3*3=9\r\n2*4=8  3*4=12  4*4=16");
    });
+it ('should return string when startnumber is 2 and endnumber is 4,the output should be something like 2*2...', () => {
+          expect(printMuiltplicationTable(2, 4)).toBe("2*2=4\r\n2*3=6  3*3=9\r\n2*4=8  3*4=12  4*4=16");
+   });
+ it ('should return null when startnumber is 5 and endnumber is 4', () => {
+            expect(printMuiltplicationTable(5, 4)).toBe(null);
+     });
+  it ('should return null when startnumber is 0 and endnumber is 1001', () => {
+              expect(printMuiltplicationTable(0, 1001)).toBe(null);
+       });
--- main.js
+++ main.js
@@ -6,17 +6,30 @@ function isInRangeOfOneToThousand(startnumber, endnumber) {
    }
 
 function createMuiltiplicationResult(startnumber, endnumber) {
-       var str=" "
-       for(var startnumber=2;startnumber<=4;startnumber++)
-       {
-            for(var endnumber=2;endnumber<=startnumber;endnumber++)
-            {
-                 str+=endnumber+"*"+startnumber+"="+startnumber*endnumber+"&nbsp;"+"&nbsp;";
-             }
-    str+="<br />"
-    }
+
+      var str=""
+           for(var startnumber=2;startnumber<=4;startnumber++)
+           {
+                for(var endnumber=2;endnumber<=startnumber;endnumber++)
+                {
+                     str+=endnumber+"*"+startnumber+"="+startnumber*endnumber+" "+" ";
+                 }
+        str+="\n"
+        }
     return str;
-}
+   }
+function printMuiltplicationTable(startnumber,endnumber){
+   if(startnumber>endnumber){
+        return null;
+   }
+   if(!(isInRangeOfOneToThousand(startnumber,endnumber)))
+        return null;
+    var  result= createMuiltiplicationResult(startnumber,endnumber);
+   return result;
+   }
+
+
 module.exports = isStartNumberSmallThanEndNumber;
 module.exports = isInRangeOfOneToThousand;
 module.exports = createMuiltiplicationResult;
+module.exports = printMuiltplicationTable;
